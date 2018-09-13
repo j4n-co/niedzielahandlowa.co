a12eedd7cb26c4a6fe99a6e0ba694f2723b58407
Jan Drewniak <jan.drewniak@gmail.com>
2018-09-13 18:17:42 +0200

    switching over to handlbars templates.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const closedDays = require( './dates.json' );
+const fs = require( 'fs' );
+const Handlebars = require( 'handlebars' );
 const monthNames = {
 	1:'Styczeń',
 	2:'Luty',
@@ -12,9 +14,7 @@ const monthNames = {
 	10:'Październik',
 	11:'listopad',
 	12:'Grudzień'
-}
-
-const fs = require( 'fs' );
+};
 
 class FancyDate {
 	constructor( dateString, closedDates ) {
@@ -31,15 +31,37 @@ class FancyDate {
 	get tomorrow() { return new Date( this.Date.getTime() + (24 * 60 * 60 * 1000) ) }
 	get isSunday() {  return this.dayOfWeek === 7 };
 	get isToday() {
-
 		var bool = ( this.now.getUTCFullYear() === this.Date.getUTCFullYear() )
 		&& (this.now.getUTCMonth() === this.Date.getUTCMonth() )
 		&& ( this.now.getUTCDate() === this.Date.getUTCDate() );
 		return bool;
 	}
+	get styleAttr() {
+		var styleAttr;
+		if ( this.dayOfMonth === 1 ) {
+			styleAttr = `style="grid-column-start: ${this.dayOfWeek};"`
+		}
 
+		return styleAttr;
+	}
+	get classAttr() {
+		var classList = [];
+		var classAttr;
+		if ( this.isSunday && this.isOpen  ) {
+			classList.push( 'open' );
+		} else if ( this.isSunday ) {
+			classList.push( 'closed' )
+		}
+
+		if ( this.isToday ) {
+			classList.push( 'today' )
+		}
+		if ( classList.length ) {
+			classAttr = `class="${classList.join(' ')}"`;
+		}
+		return classAttr;
+	}
 }
-
 
 function range( start, end ) {
 	return Array.from(Array(end - start + 1).keys()).map(i => i + start);
@@ -54,56 +76,30 @@ function daysInRange( startDate, endDate ) {
 	return Math.round(msDifference/ONE_DAY)
 }
 
-const data = createCalendarData( 'March 1 2018 00:00:00 GMT+00:00', 'December 31 2019 00:00:00 GMT+00:00', closedDays )
-
 function createCalendarData( start, end, closedDays ) {
 
 	var startDate = new FancyDate( start );
 	var endDate = new FancyDate( end );
 	var numberOfDays = daysInRange( startDate.Date, endDate.Date );
 	var tmpDate = new FancyDate( start );
-	var data = {};
+	var data = { allMonths:{} };
 
 	range(0, numberOfDays).forEach( () => {
 		var year = tmpDate.year;
 		var month = tmpDate.month;
+		var monthId = `${year}-${month}`;
+		var nextMonthId = `${year}-${parseInt(month)+1}`;
+		var prevMonthId = `${year}-${parseInt(month)-1}`;
 
 		if ( data[year] === undefined ) {
 			data[year] = {};
 		}
 
 		if ( data[year][month] === undefined ) {
-			data[year][month] = [];
-		}
 
-		data[year][month].push( tmpDate );
-		tmpDate = new FancyDate( tmpDate.tomorrow, closedDays );
+			data[year][month] = {};
+			data[year][month]['days'] = [];
 
-	});
-	return data;
-}
-
-function calendarTemplate( calendarData ) {
-	var tmp = '';
-	tmp += `
-	<div>`;
-	tmp += Object.keys( calendarData ).reduce( ( tmp, year ) => {
-		tmp += `
-		<label for="calendar-${year}">${year}</label>
-		`;
-		return tmp;
-	}, '' );
-	tmp += `
-	</div>
-	`;
-	tmp += Object.keys( calendarData ).reduce( ( tmp, year ) => {
-		const yearData = calendarData[year];
-		tmp += Object.keys( yearData ).reduce( (tmp, month, i) => {
-			const monthData = yearData[month];
-			const monthId = `${year}-${month}`;
-
-			let nextMonthId = `${year}-${parseInt(month)+1}`;
-			let prevMonthId = `${year}-${parseInt(month)-1}`
 			if ( ( parseInt( month ) + 1 ) > 12 ) {
 				nextMonthId = `${parseInt(year) + 1}-1`;
 			}
@@ -111,98 +107,54 @@ function calendarTemplate( calendarData ) {
 				prevMonthId = `${parseInt(year) - 1}-12`;
 			}
 
-			return tmp += `
-		<time class="calendar-unit" id="${monthId}" datetime="${monthId}">
-			<h3 class="month-name">
-				Niedziele handlowe <b>${monthNames[ month ]} ${year}</b>
-			</h3>
-			<a class="month-prev-link" href="#${prevMonthId}"></a>
-			<a class="month-next-link" href="#${nextMonthId}"></a>
-			<div class="calendar">
-			${ monthData.reduce( (tmp, day) => {
-				var classList = [];
-				var classAttr = '';
-				var styleAttr = '';
-				if ( day.isSunday && day.isOpen  ) {
-					classList.push( 'open' );
-				} else if ( day.isSunday ) {
-					classList.push( 'closed' )
-				}
-				if ( day.dayOfMonth === 1 ) {
-					styleAttr = `style="grid-column-start: ${day.dayOfWeek};"`
-				}
-				if ( day.isToday ) {
-					classList.push( 'today' )
-				}
-				if ( classList.length ) {
-					classAttr = `class="${classList.join(' ')}"`;
-				}
+			data[year][month].monthId = monthId;
+			data[year][month].prevMonthId = prevMonthId;
+			data[year][month].nextMonthId = nextMonthId;
+			data[year][month].monthName = monthNames[ month ];
 
-				return tmp += `
-				<time datetime="${day.year}-${day.month}-${day.dayOfMonth}" ${classAttr} ${styleAttr}>${day.dayOfMonth}</time>`
-			}, '' ) }
-			</div>
-		</time>
-		`;
-		}, '')
-		return tmp;
-	}, '');
-	return tmp;
+		}
+		if ( data.allMonths[monthId] === undefined ) {
+			data.allMonths[monthId] = {};
+			data.allMonths[monthId].prevMonthId = prevMonthId;
+			data.allMonths[monthId].nextMonthId = nextMonthId;
+			data.allMonths[monthId].monthName = `${monthNames[ month ]} ${year}`;
+			data.allMonths[monthId].monthId = monthId;
+			data.allMonths[monthId].days = [];
+
+		}
+		data.allMonths[monthId].days.push( tmpDate );
+		data[year][month].days.push( tmpDate );
+		tmpDate = new FancyDate( tmpDate.tomorrow, closedDays );
+	});
+	return data;
 }
 
-function htmlTemplate( innerTemplate ) {
-	return `
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Niedziela Handlowa</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-	<script>
-	function onLoad(el) {
-		var now = new Date();
-		var id =  now.getFullYear() + '-' + ( now.getMonth() + 1 );
-		var el = document.getElementById( id );
-		el.scrollIntoView( false );
+function generateFile( data ) {
+	var source = fs.readFileSync( 'template.handlebars', 'utf8');
+	var template = Handlebars.compile(source);
+	var today = new FancyDate( new Date() );
+	var headline = 'Czy sklepy będą otwarte w niedziele?';
+	var yesno = "NIE";
+	var nextSunday = new FancyDate( new Date().setDate( today.Date.getDate() + ( 7 - today.dayOfWeek ) ) );
+	if (today.isSunday ) {
+		headline = "Czy sklepy są otwarte dzisiaj?"
+		if ( today.isOpen ) {
+			yesno = 'TAK';
+		}
 	}
-</script>
-</head>
-<body onload="onLoad()">
-	<h1 class="headline">
-		Czy sklepy będą jutro otwarte?
-	</h1>
-	<h2 class="yesno no">
-		NIE!
-	</h2>
-	<div class="calendar-scroll-container">
-${innerTemplate}
-	</div>
+	if (new FancyDate( today.tomorrow ).isSunday ) {
+		headline = "Czy sklepy będą jutro otwarte?"
+	}
 
-	<div class="month-links">
-		<h4 class="subheading">2018</h4>
-		<a class="month-link" href="">01</a>
-		<a class="month-link" href="">02</a>
-		<a class="month-link" href="">03</a>
-		<a class="month-link" href="">04</a>
-		<a class="month-link" href="">05</a>
-		<a class="month-link" href="">06</a>
-		<a class="month-link" href="">07</a>
-		<a class="month-link" href="">08</a>
-		<a class="month-link" href="">09</a>
-		<a class="month-link" href="">10</a>
-		<a class="month-link" href="">11</a>
-		<a class="month-link" href="">12</a>
-	</div>
-</div>
-</body>
-</html>
-`;
+	var templateData = {
+		calendarUnits: data,
+		allMonths: data.allMonths,
+		mainHeadline: headline,
+		yesno: ( nextSunday.isOpen ) ? 'TAK' : 'NIE'
+	}
+	var result = template( templateData );
+	fs.writeFileSync( 'handlebars-test.html', result );
 }
 
-var calendarTmpl = calendarTemplate( data );
-var htmlTmpl = htmlTemplate( calendarTmpl )
-
-fs.writeFileSync( 'test.html', htmlTmpl );
-
+const data = createCalendarData( 'March 1 2018 00:00:00 GMT+00:00', 'December 31 2019 00:00:00 GMT+00:00', closedDays )
+generateFile( data );
